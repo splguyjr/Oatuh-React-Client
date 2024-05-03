4e1ace310b1b3095842380c3dfab804bd71f6a19
WoonKyung Kim <splguyjr@khu.ac.kr>
2024-05-04 03:02:13 +0900

    Oauth_React_Client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from "./LoginPage";
+import KakaoRedirectPage from "./KakaoRedirectPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='App'>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage />}></Route>
+            <Route path="/oauth/redirected/kakao" element={<KakaoRedirectPage />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
   );
-}
+};
 
 export default App;
